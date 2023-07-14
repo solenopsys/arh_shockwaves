d9d373217bff76902be60412d3fe58bf293e72ee
AlexStorm <info@alexstorm.ru>
2023-07-14 16:57:05 +0530

    proxy improve

--- cmd/serve/front.go
+++ cmd/serve/front.go
@@ -2,15 +2,33 @@ package serve
 
 import (
 	"github.com/spf13/cobra"
+	"strconv"
+	"sync"
 	"xs/pkg/io"
 )
 
 var cmdFront = &cobra.Command{
 	Use:   "front [name]",
 	Short: "Frontend serve",
-	Args:  cobra.MinimumNArgs(0),
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		io.StartProxy("C:\\dev\\sources\\MAIN\\temp2\\frontends\\dist\\fronts\\fr-web")
+		count := len(args)
 
+		port := 8000
+
+		wg := new(sync.WaitGroup)
+		for i := 0; i < count; i++ {
+
+			name := args[i]
+
+			strPort := strconv.Itoa(port)
+
+			io.Println("Print: " + name + ": " + strPort)
+			go io.StartProxy(".\\dist\\fronts\\"+name, strPort)
+			port++
+			wg.Add(1)
+		}
+
+		wg.Wait()
 	},
 }
--- pkg/io/proxy.go
+++ pkg/io/proxy.go
@@ -3,17 +3,68 @@ package io
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
-func StartProxy(staticDir string) {
+func StartProxy(staticDir string, port string) {
+
+	mux := http.NewServeMux()
 
 	// Create a file server handler
 	fileServer := http.FileServer(http.Dir(staticDir))
 
+	targetURL, _ := url.Parse("http://solenopsys.org/")
+
+	// Create a reverse proxy with custom Director function
+	reverseProxy := &httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			req.URL.Scheme = targetURL.Scheme
+			req.URL.Host = targetURL.Host
+			req.URL.Path = "/dag"
+			req.Host = targetURL.Host
+		},
+	}
+
+	mux.Handle("/dag", reverseProxy)
+
 	// Serve static files for any path
-	http.Handle("/", fileServer)
+	mux.Handle("/", fileServer)
 
 	// Start the server
-	log.Println("Static file server listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+// LogTransport is a custom transport that logs the HTTP requests and responses
+type LogTransport struct {
+	Transport http.RoundTripper
+}
+
+// RoundTrip executes the HTTP request and logs the details
+func (lt *LogTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Log the request details
+	requestDump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		log.Println("Error logging request:", err)
+	}
+	log.Println("Request:")
+	log.Println(string(requestDump))
+
+	// Execute the request using the underlying transport
+	response, err := lt.Transport.RoundTrip(req)
+	if err != nil {
+		log.Println("Error executing request:", err)
+		return nil, err
+	}
+
+	// Log the response details
+	responseDump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Println("Error logging response:", err)
+	}
+	log.Println("Response:")
+	log.Println(string(responseDump))
+
+	return response, nil
 }
