d11d7210e89eae702871df786cb144e82b5f5795
AlexStorm <info@alexstorm.ru>
2023-09-15 12:15:15 +0530

    env status fix

--- cmd/env/status.go
+++ cmd/env/status.go
@@ -11,19 +11,13 @@ var cmdStatus = &cobra.Command{
 	Short: "Show status of installed env programs (git,pnpm,go,...)",
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		var commands []jobs_env.AppCheck
-		commands = append(commands, *jobs_env.NewAppCheck("git", []string{"git", "version"}))
-		commands = append(commands, *jobs_env.NewAppCheck("pnpm", []string{"pnpm", "-v"}))
-		commands = append(commands, *jobs_env.NewAppCheck("go", []string{"go", "version"}))
-		commands = append(commands, *jobs_env.NewAppCheck("ng-packagr", []string{"ng-packagr", "-v"}))
-		commands = append(commands, *jobs_env.NewAppCheck("nerdctl", []string{"nerdctl", "version"}))
-
-		var ex []jobs.Job
-
-		for _, printableJob := range commands {
-			var job jobs.Job = &printableJob
-			ex = append(ex, job)
+		var jobsPlan = []jobs.Job{
+			jobs_env.NewAppCheck("git", []string{"git", "version"}),
+			jobs_env.NewAppCheck("pnpm", []string{"pnpm", "-v"}),
+			jobs_env.NewAppCheck("go", []string{"go", "version"}),
+			jobs_env.NewAppCheck("ng-packagr", []string{"ng-packagr", "-v"}),
+			jobs_env.NewAppCheck("nerdctl", []string{"nerdctl", "version"}),
 		}
-		jobs.ExecuteJobsSync(ex)
+		jobs.ExecuteJobsSync(jobsPlan)
 	},
 }
